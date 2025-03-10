9a027ea69a4b46ea60d1d7684254f1d02f7732e3
Nikenzsk6 <ajaniken307@gmail.com>
2025-03-10 09:32:03 +0700

    penambahan nama dan nim

--- src/njghb/Njghb.java
+++ src/njghb/Njghb.java
@@ -15,7 +15,9 @@ public class Njghb {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hello");
+        System.out.println("Niken Ayu Zaskia F.");
+        System.out.println("Hello Word");
+        System.out.println("(202457201057)");
     }
     
 }
